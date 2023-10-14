a5aa9889860be9f7594205668935f65fcef4b807
Gogen2003 <122785869+Gogen2003@users.noreply.github.com>
2023-10-14 14:16:36 +0300

    dw
    dw

---
+++ app_icons/src/Content.js
@@ -0,0 +1,39 @@
+
+import {Route, Routes} from 'react-router-dom'
+import Test from './Test';
+import Glavnaya from './Glavnaya';
+import Photo from './Photo';
+import Knopka_otziv from './Knopka_otziv';
+import Contacts from './Contacts'
+import Uslugi from './Uslugi'
+
+
+function Content() {
+    
+    
+    
+    
+    
+    return (
+        <>
+            <div id='CONTENT' className="content">
+                
+                <Routes>
+                    <Route path='/' element = {<Glavnaya />}></Route>
+                    <Route path='Test' element = {<Test />}></Route>
+                    <Route path='photo' element = {<Photo />}></Route>
+                    <Route path='otziv' element = {<Knopka_otziv />}></Route>
+                    <Route path='contacts' element = {<Contacts />}></Route>
+                    <Route path='uslugi' element = {<Uslugi />}></Route>
+                    
+                </Routes>
+            
+            </div>
+
+
+            
+        </>
+    )
+}
+
+export default Content;
---
+++ app_icons/src/Uslugi.js
@@ -0,0 +1,121 @@
+import './Design/Uslugi.css'
+function Uslugi() {
+    return (
+        <>
+            <div className="main-box">
+                <div className="mainn-content">
+                    <header>
+                        <div className="box-header">
+                            <div className='header-content'>
+                                
+                            </div>
+
+                        </div>
+                        <div className='box-main'>
+                        </div>
+                    </header>
+                    <main>
+                        <div className='box-main' >
+                            <div className='main-content' >
+                                <section id='1'>
+                                    <div className='zagalovok'><h2 id='qqq' >КЛАССИЧЕСКИЙ МАССАЖ</h2></div>
+                                    <div className='section1-content'>
+                                        <div className='img-yes'><img  src='https://static.tildacdn.com/tild3234-3339-4936-b162-633162303132/image.png'/></div>
+                                        <div className='not-img' >
+                                            <div className='about-massage'>
+                                                <div className='about-massage-content'>
+                                                    <div  className='about-massage-zagolovok'><h2>О массаже</h2></div>
+                                                    <div><p><span style={{opacity: '0'}}>32322</span>Классический массаж – один из самых современных методов лечения и<br/>профилактики множества заболеваний. Классический массаж не только<br/>
+                                                        снимает боль, отеки, спайки, нормализует функции органов и систем,<br/> но и ускоряет процессы регенерации травмированных тканей,восстанавливает<br/>
+                                                        тонус мышц, повышает работоспособность костно-мышечной системы,<br/>укрепляет суставно-связочный аппарат,<br/> оказывая общеукрепляющее действие на организм.<br/></p> 
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className='prices'>
+                                                <div className='prices-content'>
+                                                    <div className='prices-zagolovok'><h2>Цены:</h2></div>
+                                                    <div className='prices-table'>
+                                                        <ul>
+                                                            <li style = {{listStyle: 'none'}}>30 минут: <strike>1500р</strike> 1250р</li>
+                                                            <li style = {{listStyle: 'none'}}>1 час: <strike>3000</strike> 2500р</li>
+                                                            <li style = {{listStyle: 'none'}}>10 сеансов(1час): <strike>30000р</strike> 28000р </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>
+                                <section id='2'>
+                                <div className='zagalovok'><h2 id='qqq' >МАССАЖ ЛИЦА</h2></div>
+                                <div className='section1-content'>
+                                        <div className='img-yes'><img  src='https://tyumen.ultrapodarki.ru/upload/resize_cache/iblock/e3b/846_400_1/e3bc8f5ab87cbcac8dfd7b20a871fb38.jpg'/></div>
+                                        <div className='not-img' >
+                                            <div className='about-massage'>
+                                                <div className='about-massage-content' >
+                                                    <div  className='about-massage-zagolovok'><h2>О массаже</h2></div>
+                                                    <div><p><span style={{opacity: '0'}}>32322</span>Массаж лица – это популярная косметологическая процедура, которая <br/>помогает улучшить состояние кожи. В ходе его проведения удается улучшить<br/> кровообращение, отток лимфы, повысить тургор эпидермиса. Включать массаж<br/> в уход за лицом рекомендуется с 25 лет, а после 30 процедуру стоит делать регулярно.</p> 
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className='prices'>
+                                                <div className='prices-content'>
+                                                    <div  className='prices-zagolovok'><h2>Цены:</h2></div>
+                                                    <div className='prices-table'>
+                                                        <ul>
+                                                            <li style = {{listStyle: 'none'}}>30 минут: <strike>1500р</strike> 1250р</li>
+                                                            <li style = {{listStyle: 'none'}}>1 час: <strike>3000</strike> 2500р</li>
+                                                            <li style = {{listStyle: 'none'}}>10 сеансов(1час): <strike>30000р</strike> 28000р </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                </div>
+                                </section>
+                                <section id='3'>
+                                    <div className='zagalovok'><h2 id='qqq' >МАНУАЛЬНАЯ ТЕРПАИЯ</h2></div>
+                                        <div className='section1-content' style={{marginBottom: '4%'}}>
+                                            <div className='img-yes'><img  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHEO5SMS24IzjTI8DTtg1rl8LZCKnopJ4YHA&usqp=CAU'/></div>
+                                            <div className='not-img' >
+                                                <div className='about-massage'>
+                                                    <div className='about-massage-content'>
+                                                        <div  className='about-massage-zagolovok'><h2>О массаже</h2></div>
+                                                        <div><p><span style={{opacity: '0'}}>32322</span>Классический массаж – один из самых современных методов лечения и<br/>профилактики множества заболеваний. Классический массаж не только<br/>
+                                                            снимает боль, отеки, спайки, нормализует функции органов и систем,<br/> но и ускоряет процессы регенерации травмированных тканей,восстанавливает<br/>
+                                                            тонус мышц, повышает работоспособность костно-мышечной системы,<br/>укрепляет суставно-связочный аппарат,<br/> оказывая общеукрепляющее действие на организм.<br/></p> 
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className='prices'>
+                                                    <div className='prices-content'>
+                                                        <div  className='prices-zagolovok'><h2>Цены:</h2></div>
+                                                        <div className='prices-table'>
+                                                            <ul>
+                                                                <li style = {{listStyle: 'none'}}>30 минут: <strike>1500р</strike> 1250р</li>
+                                                                <li style = {{listStyle: 'none'}}>1 час: <strike>3000</strike> 2500р</li>
+                                                                <li style = {{listStyle: 'none'}}>10 сеансов(1час): <strike>30000р</strike> 28000р </li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                </section>
+                            </div>
+                        </div>
+                    </main>
+                    <footer>
+                        <div className='box-footer'>
+                            <div className='footer-content'>
+
+                            </div>
+                        </div>
+                    </footer>
+
+                </div>
+            </div>
+        </>
+    )
+}
+export default Uslugi;
